d0779a883abcc74bf2d910ba7542ee34fa41c446
ChangHyunLee <tooseve@gmail.com>
2016-09-02 13:17:55 +0900

    주석

--- Git_lch_3009/src/com/bitacadamy/bit/HelloGit.java
+++ Git_lch_3009/src/com/bitacadamy/bit/HelloGit.java
@@ -5,6 +5,12 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("안녕 Git");
 		System.out.println("안녕");
+		
+		//주석 주석 주석 주석 주석 주석
+		
+		// 주석주석주석2
+		
+		// 주석주석3-저장-커밋
 
 	}
 
